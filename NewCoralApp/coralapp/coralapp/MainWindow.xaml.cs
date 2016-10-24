aa091e7f5fadcc11a3549f225300c11204e11c6d
polianna <polina.kraskova@gmail.com>
2016-10-24 16:25:58 +0300

    First commit

---
+++ NewCoralApp/coralapp/coralapp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace coralapp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        
+
+        private void tbSearchProductName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            tbSearchProductName.Clear();
+        }
+       
+            private void tbSearchProductCode_GotFocus(object sender, RoutedEventArgs e)
+        {
+            tbSearchProductCode.Clear();
+        }
+    }
+}
---
+++ NewCoralApp/coralapp/coralapp/Window1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace coralapp
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        checkin childForm; //Запоминаем ссылку на дочернюю форму
+ 
+        private void hellosorry_Click(object sender, EventArgs e)
+        {
+            if (childForm != null)
+                childForm.Close(); //если форма открыта - закрываем
+            OpenChildForm();
+        }
+ 
+        private void OpenChildForm() {
+                childForm = new checkin();
+                childForm.Closed += (s, e) => childForm = null; //При закрытии формы обнуляем ссылку
+                childForm.Show();
+        }
+
+        private void tbLogin_GotFocus(object sender, RoutedEventArgs e)
+        {
+            tbLogin.Clear();
+        }
+
+        private void tbPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            tbPassword.Clear();
+        }
+    }
+}
