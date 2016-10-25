c1a89b4039f67fe5fef652e748a29c9ad5f03847
polianna <polina.kraskova@gmail.com>
2016-10-25 22:45:07 +0300

    Добавила функцию отправки почты

--- NewCoralApp/coralapp/coralapp/checkin.xaml.cs
+++ NewCoralApp/coralapp/coralapp/checkin.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +24,42 @@ namespace coralapp
         public checkin()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(sendit("polina.kraskova@gmail.com","Тест","Тест"));
+        }
+
+        public string sendit(string ReciverMail, string subject, string message)
+        {
+            MailMessage msg = new MailMessage();
+
+            msg.From = new MailAddress("coralappamail@gmail.com");
+            msg.To.Add(ReciverMail);
+            msg.Subject = subject;
+            msg.Body = message;
+            SmtpClient client = new SmtpClient();
+            client.UseDefaultCredentials = true;
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Credentials = new NetworkCredential("coralappmail@gmail.com", "privetpolina123");
+            client.Timeout = 20000;
+            try
+            {
+                client.Send(msg);
+                return "Mail has been successfully sent!";
+            }
+            catch (Exception ex)
+            {
+                return "Fail Has error" + ex.Message;
+            }
+            finally
+            {
+                msg.Dispose();
+            }
         }
     }
 }
