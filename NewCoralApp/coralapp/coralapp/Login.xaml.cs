5359a78e863f030ca392789ae9d50ce35a531edc
polianna <polina.kraskova@gmail.com>
2016-10-26 12:07:13 +0300

    Добавила авторизацию

--- NewCoralApp/coralapp/coralapp/Login.xaml.cs
+++ NewCoralApp/coralapp/coralapp/Login.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +23,52 @@ namespace coralapp
     /// </summary>
     public partial class Window1 : Window
     {
+        string connectionString;
+        SqlDataAdapter adapter;
+
         public Window1()
         {
             InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        static bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = GetMd5Hash(md5Hash, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         checkin childForm; //Запоминаем ссылку на дочернюю форму
@@ -43,10 +90,61 @@ namespace coralapp
         {
             tbLogin.Clear();
         }
+        
 
-        private void tbPassword_GotFocus(object sender, RoutedEventArgs e)
+        private void bLogin_Click(object sender, RoutedEventArgs e)
         {
-            tbPassword.Clear();
+            SqlConnection connection = null;
+            SqlDataReader reader = null;
+            string dbPass = "";
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand("select pass from [User] where username = @username", connection);
+                command.Parameters.Add("username", SqlDbType.NVarChar).Value = tbLogin.Text;
+                connection.Open();
+                reader = command.ExecuteReader();
+
+                // write each record
+                while (reader.Read())
+                {
+                    dbPass = reader.GetString(0);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+
+                if (connection != null)
+                    connection.Close();
+            }
+
+            using (MD5 md5Hash = MD5.Create())
+            {
+                bool isLogin = VerifyMd5Hash(md5Hash, pbPassword.Password, dbPass);
+                if (isLogin)
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Auth wrong");
+                }
+            }
+        }
+
+        private void pbPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            pbPassword.Clear();
         }
     }
 }
