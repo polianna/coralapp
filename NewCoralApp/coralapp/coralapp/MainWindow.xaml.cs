07f72ec9230a1e06f2a6b561b0c2e904942a2d81
polianna <polina.kraskova@gmail.com>
2016-11-27 01:39:11 +0300

    Добавила загрузку из Excel

--- NewCoralApp/coralapp/coralapp/MainWindow.xaml.cs
+++ NewCoralApp/coralapp/coralapp/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.OleDb;
 
 namespace coralapp
 {
@@ -719,7 +720,82 @@ private void addNewSupplyDB(int priceId, int quantity) {
             }
 
         }
-        
+
+        private string GetConnectionString()
+        {
+            Dictionary<string, string> props = new Dictionary<string, string>();
+
+            // XLSX - Excel 2007, 2010, 2012, 2013
+            props["Provider"] = "Microsoft.ACE.OLEDB.12.0;";
+            props["Extended Properties"] = "Excel 12.0 XML";
+            props["Data Source"] = "C:\\Users\\xkirax\\Google Диск\\Полина\\БД товаров.xlsx";
+
+            // XLS - Excel 2003 and Older
+            //props["Provider"] = "Microsoft.Jet.OLEDB.4.0";
+            //props["Extended Properties"] = "Excel 8.0";
+            //props["Data Source"] = "C:\\MyExcel.xls";
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> prop in props)
+            {
+                sb.Append(prop.Key);
+                sb.Append('=');
+                sb.Append(prop.Value);
+                sb.Append(';');
+            }
+
+            return sb.ToString();
+        }
+
+        private DataTable ReadExcelFile()
+        {
+            string connectionString = GetConnectionString();
+
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand();
+                cmd.Connection = conn;
+
+                // Get all Sheets in Excel File
+                DataTable dtSheet = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                // Loop through all Sheets to get data
+                DataRow dr = dtSheet.Rows[0];
+                
+                    string sheetName = dr["TABLE_NAME"].ToString();
+
+                    // Get all rows from the Sheet
+                    cmd.CommandText = "SELECT * FROM [" + sheetName + "]";
+
+                    DataTable dt = new DataTable();
+                    dt.TableName = sheetName;
+
+                    OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                    da.Fill(dt);
+                
+
+                cmd = null;
+                conn.Close();
+                return dt;
+            }
+            
+        }
+
+        private void bNewUploadFromExcel_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable excel = ReadExcelFile();
+            this.newProducts = new ObservableCollection<Product>();
+            foreach (DataRow row in excel.Rows)
+            {
+                int ledger = (int)((double)row["Остаток"]);
+                if (ledger > 0) {
+                    this.newProducts.Add(new Product((string)row["Наименование"], ((double)row["Код"]).ToString(), ledger, 0));
+                }
+            }
+            dgNew.ItemsSource = this.newProducts;
+    }
     }
 
 
